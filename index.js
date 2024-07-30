c375fecd04e4c8884b7f629969ff0c0173cc2c5f
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-07-30 17:02:56 +0600

    delete query api added

--- index.js
+++ index.js
@@ -141,6 +141,14 @@ async function run() {
 			res.send(result);
 		});
 
+		// delete query api
+		app.delete('/query/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await queriesCollection.deleteOne(query);
+			res.send(result);
+		});
+
 		//get all applications api
 		app.get('/applications', async (req, res) => {
 			const result = await applicationsCollection.find().toArray();

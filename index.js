0fb932875c8f1f0885ae4a546c75e57c87394fd0
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-07-31 15:48:34 +0600

    delete application api added

--- index.js
+++ index.js
@@ -171,6 +171,14 @@ async function run() {
 			res.send(result);
 		});
 
+		// delete application api
+		app.delete('/application/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await applicationsCollection.deleteOne(query);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log("Pinged your deployment. You successfully connected to MongoDB!");
